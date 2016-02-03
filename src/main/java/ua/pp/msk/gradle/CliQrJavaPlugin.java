dea45aab227e1a0feaf2e9179ecbe7fc40cb48b3
Maksym Shkolnyi <mshkolnyi@intropro.com>
2016-02-03 14:53:53 +0200

    Some first java steps. Just trying

--- src/main/java/ua/pp/msk/gradle/CliQrJavaPlugin.java
+++ src/main/java/ua/pp/msk/gradle/CliQrJavaPlugin.java
@@ -2,12 +2,13 @@ package ua.pp.msk.gradle;
 
 import org.gradle.api.Project;
 import org.gradle.api.Plugin;
+import org.gradle.api.Task;
 
 public class CliQrJavaPlugin implements Plugin<Project> {
 
-        @Override
-            public void apply(Project target) {
-                        target.task("javaTask");
-                            }
+    @Override
+    public void apply(Project target) {
+        Task task = target.task("dummyTask");
+    }
 
 }
--- src/main/java/ua/pp/msk/gradle/RunTask.java
+++ src/main/java/ua/pp/msk/gradle/RunTask.java
@@ -5,9 +5,15 @@ import org.gradle.api.tasks.TaskAction;
 
 public class RunTask extends DefaultTask {
 
-        @TaskAction
-            public void javaTask() {
-                        System.out.println("Hello from MyJavaTask");
-                            }
+    @TaskAction
+    public void dummyTask() {
+        System.out.println("It is a very dummy plugin yet. Hello!!!");
+        //getProject().getPlugins().apply(null);
+    }
+    
+    @TaskAction
+    public void cliqrRun() {
+        System.out.println("Running CliQr Job");
+    }
 
 }
